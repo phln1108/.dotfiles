291e0e29c49f289cfcb36244358524c458d6ae25
pedro <pedronunes@unifor.br>
2023-08-04 14:44:23 -0300

    fixing the blur on vortex pc

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -9,11 +9,12 @@ run() {
 HOME=$HOME
 
 #settings for computer from vortex
-if [ HOME == "home/pedroh" ]; then
-    echo "kkkkk"
+if [ HOME=="home/pedroh" ]; then
     run "xrandr" --output DP-1 --primary --left-of VGA-1 
+    run "picom" --experimental-backends --config ~/.config/picom/picom.conf
+elif [HOME == "home/ph"  ]; then
+    run "picom" --config ~/.config/picom/picom.conf
 fi
 
-run "picom" --config ~/.config/picom/picom.conf
 
 
