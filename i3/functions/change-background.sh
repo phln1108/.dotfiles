82f32029b6b7d3e3b14fa7a63b2c2aaa5ab9e370
pedro <pedro@localhost.localdomain>
2024-01-24 09:36:23 -0300

    still configuring change background

--- i3/functions/change-background.sh
+++ i3/functions/change-background.sh
@@ -8,11 +8,11 @@ res=$(echo $gifs | rofi -sep ' ' -config $1 -dmenu);
 
 workspace=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).num' | cut -d"\"" -f2)
 
-"i3-msg" workspace 0 
+# "i3-msg" workspace 0 
 
 kitty -o allow_remote_control=yes cbonsai -l -i -m "waiting for loading background :D" &
 
-"$path/functions/i3-new-workspace.sh" -c
+# "$path/functions/i3-new-workspace.sh" -c
 
 
 $path/functions/generate_pngs.sh $path/gifs/$res $path/animated_background/
